cfc3a1b438f9f283269263d9396ed7080bc0bcd2
fuller2580 <fuller2580@gmail.com>
2016-10-06 04:16:05 -0500

    Base Game Idea

---
+++ Assets/followMouse.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followMouse : MonoBehaviour {
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		Vector3 pos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10));
+		this.gameObject.transform.position = pos;
+	}
+}
---
+++ Assets/lightScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightScript : MonoBehaviour {
+	GameObject flashlight;
+	float maxLightDist = .1f;
+	Color OGCol;
+	public SpriteRenderer spriteRenderer = null;
+	// Use this for initialization
+	void Start () {
+		flashlight = GameObject.FindGameObjectWithTag("LightObj");
+		if(!spriteRenderer) spriteRenderer = this.GetComponent<SpriteRenderer>();
+		OGCol = spriteRenderer.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = 1/Vector3.SqrMagnitude(transform.position-flashlight.transform.position);
+		if(distance < maxLightDist) {
+			spriteRenderer.color = new Vector4(OGCol.r,OGCol.g,OGCol.b,0);
+		}
+		else {
+			spriteRenderer.color = new Vector4(OGCol.r,OGCol.g,OGCol.b,(distance));
+		}
+	}
+}
