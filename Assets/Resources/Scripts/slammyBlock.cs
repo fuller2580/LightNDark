4b6d124b514713b2dd1d85beb3d8bc2da4a6d52b
fuller2580 <fuller2580@gmail.com>
2016-10-31 22:28:43 -0500

    Slam Block Implemented
    Set start location of block.
    
    set slam target location with target gameobject.
    
    Can adjust drop and return speeds.
    
    pause time changes how long block waits after slamming to return to
    start location.
    
    Slam activates only when block is lit up.

---
+++ Assets/Resources/Scripts/slammyBlock.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slammyBlock : MonoBehaviour {
+	public GameObject TargetA;
+	Vector3 TargetB;
+	bool goToA = true;
+	bool working = false;
+	public float dropSpeed = 1f;
+	public float returnSpeed = 1f;
+	public float pauseTime = 0.0f;
+	bool canMove = false;
+	bool activate = false;
+	public SpriteRenderer spriteRenderer = null;
+
+	// Use this for initialization
+	void Start () {
+		TargetB = transform.position;
+		if(TargetA){
+			canMove = true;
+		}
+		else print("missing Target GameObject for a slam block");
+		if(!spriteRenderer) spriteRenderer = this.GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(canMove && activate){
+			if(goToA)transform.position = Vector3.MoveTowards(transform.position, TargetA.transform.position, (Time.deltaTime * dropSpeed));
+			else transform.position = Vector3.MoveTowards(transform.position, TargetB, (Time.deltaTime * returnSpeed));
+
+			if(transform.position == TargetA.transform.position && goToA) StartCoroutine(startToB());
+			else if(transform.position == TargetB && !goToA) StartCoroutine(startToA());
+		}
+		else{
+			if(spriteRenderer.color.a >= 0.1f) activate = true;
+		}
+	}
+
+	IEnumerator startToA(){
+		if(!working){
+			activate = false;
+			working = true;
+			yield return new WaitForSeconds(0);
+			goToA = true;
+			working = false;
+		}
+	}
+
+	IEnumerator startToB(){
+		if(!working){
+			
+			working = true;
+			yield return new WaitForSeconds(pauseTime);
+			goToA = false;
+			working = false;
+		}
+	}
+}
