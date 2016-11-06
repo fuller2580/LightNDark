8c4a50ec167d0a26881ffa01d916d5b4cb06b282
fuller2580 <fuller2580@gmail.com>
2016-11-06 17:06:42 -0600

    Fishy Fish
    started working on fish functionality.
    ----
    Range is max distance it can move from start position (x and y).
    
    speed is how fast it moves (adjusted by framerate every so often).
    
    animation stuff for moving in each direction.
    
    ----

---
+++ Assets/Resources/Scripts/fishy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishy : MonoBehaviour {
+	Vector2 centerPoint;
+	Vector2 targetPos;
+	public float range = 10f;
+	public float speed = 2f;
+	float moveSpeed;
+	bool canMove = false;
+	// Use this for initialization
+	void Start () {
+		centerPoint = new Vector2(this.gameObject.transform.position.x,this.gameObject.transform.position.y);
+		StartCoroutine(pickTarget());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(canMove){
+			if(new Vector2(transform.position.x,transform.position.y) == targetPos){
+				canMove = false;
+				StartCoroutine(pickTarget());
+			}
+			else transform.position = Vector2.MoveTowards(new Vector2(transform.position.x,transform.position.y),targetPos,moveSpeed);
+		}
+	}
+
+	IEnumerator pickTarget(){
+		yield return new WaitForSeconds(1);
+		targetPos = (new Vector2(Random.Range(-range,range),Random.Range(-range,range)) + centerPoint);
+		Vector3 myScale = transform.localScale;
+		if(targetPos.x > transform.position.x) myScale.x = -1;
+		else myScale.x = 1;
+		transform.localScale = myScale;
+		moveSpeed = speed * Time.deltaTime;
+		canMove = true;
+	}
+}
